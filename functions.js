e80f53f44edb152c19bbd861ed5235a78c92e1cb
Mohammed Hamdan <mohammed.tnh@gmail.com>
2021-01-11 10:55:20 +0300

    Pass

--- functions.js
+++ functions.js
@@ -8,6 +8,7 @@
  */
 function greet(name) {
   // Your code here
+  console.log(`Hello ${name}`);
 }
 
 /**
@@ -21,6 +22,11 @@ function greet(name) {
  */
 function isOdd(n) {
   // Your code here
+  if (n % 2 === 1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -34,6 +40,11 @@ function isOdd(n) {
  */
 function oddsSmallerThan(n) {
   // Your code here
+  if (n === 0) {
+    return n;
+  } else {
+    return parseInt(n / 2);
+  }
 }
 
 /**
@@ -48,6 +59,11 @@ function oddsSmallerThan(n) {
  */
 function squareOrDouble(n) {
   // Your code here
+  if (n % 2 === 1) {
+    return Math.pow(n, 2);
+  } else {
+    return n * 2;
+  }
 }
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
